20321d04c93225c68c8260a8bf7a678909ead36d
Abelardo <admendoza@indracompany.com>
2020-01-07 11:17:44 -0500

    RestServer - usuario

---
+++ server/server.js
@@ -0,0 +1,23 @@
+require('./config/config');
+const mongoose = require('mongoose');
+
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+ 
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(require('./routes/usuario'));
+
+mongoose.connect('mongodb://localhost:27017/cafe', 
+	{useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true},
+	(err, res) =>{
+	if(err) throw err;
+	console.log('Base de datos ONLINE');
+});
+
+app.listen(port, () => console.log(`Estamos escuchando en el puerto ${port}`));
